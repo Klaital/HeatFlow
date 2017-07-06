0812677489c0f95465d945d88eac8d1b6fc94aa0
Chris Cox <chriscox@abandonedfactory.net>
2017-07-06 11:41:09 -0700

    Removing leading tabs

--- src/HeatFlowProject.h
+++ src/HeatFlowProject.h
@@ -23,14 +23,14 @@ private:    // Member variables
     std::string  notes_;
     std::string  initial_temps_matrix_path_;
     std::string  materials_matrix_path_;
-	MatrixFile<temperature_t> initial_temperatures_;
-	MatrixFile<int> materials_;
+    MatrixFile<temperature_t> initial_temperatures_;
+    MatrixFile<int> materials_;
 
-	std::map<int, Material> bom_;
+    std::map<int, Material> bom_;
 
     distance_t      field_gap_distance_; // the distance between nodes in the X direction in meters
     time_delta_t    time_step_;  // number of milliseconds between calculation iterations
-    
+
 public:     // Accessors
     inline std::string get_project_config_filename() const { return this->project_config_filename_; }
     inline void set_project_config_filename(std::string& new_filename) { this->project_config_filename_ = new_filename; }
@@ -45,25 +45,25 @@ public:     // Accessors
     inline time_delta_t get_time_step() const { return this->time_step_; }
     inline void set_time_step(time_delta_t time_step) { this->time_step_ = time_step; }
 
-	inline std::string get_initial_temps_path() const { return this->initial_temps_matrix_path_; }
-	inline void        set_initial_temps_path(const std::string& initial_temps_path) { this->initial_temps_matrix_path_ = initial_temps_path; }
-	inline std::string get_materials_path() const { return this->materials_matrix_path_; }
-	inline void        set_materials_path(const std::string& materials_path) { this->materials_matrix_path_ = materials_path; }
+    inline std::string get_initial_temps_path() const { return this->initial_temps_matrix_path_; }
+    inline void        set_initial_temps_path(const std::string& initial_temps_path) { this->initial_temps_matrix_path_ = initial_temps_path; }
+    inline std::string get_materials_path() const { return this->materials_matrix_path_; }
+    inline void        set_materials_path(const std::string& materials_path) { this->materials_matrix_path_ = materials_path; }
 
-	inline MatrixFile<temperature_t>& get_initial_temps() const { return this->initial_temperatures_; }
-	inline MatrixFile<int>&           get_materials() const { return this->materials_; }
-	
-	inline std::map<int, Material> get_bom() const { return this->bom_; }
-	inline void set_bom(const std::map<int, Material>& new_bom) { this->bom_ = new_bom; }
+    inline MatrixFile<temperature_t>& get_initial_temps() const { return this->initial_temperatures_; }
+    inline MatrixFile<int>&           get_materials() const { return this->materials_; }
 
-public:		// Data Manipulation
-	int add_material(const Material& new_material);
-	int delete_material(const std::string& name);
-	int delete_material(int index);
-	void copy_materials_matrix(MatrixFile<Material *> &materials_matrix) const;
+    inline std::map<int, Material> get_bom() const { return this->bom_; }
+    inline void set_bom(const std::map<int, Material>& new_bom) { this->bom_ = new_bom; }
+
+public:     // Data Manipulation
+    int add_material(const Material& new_material);
+    int delete_material(const std::string& name);
+    int delete_material(int index);
+    void copy_materials_matrix(MatrixFile<Material *> &materials_matrix) const;
 
 public:     // Constructors & Destructors
-    
+
     // Construct an empty project
     HeatFlowProject();
     
