0e6ff9b086e7fd317e41dd9fe063abff66b0302d
Chris Cox <chriscox@abandonedfactory.net>
2017-07-11 10:26:02 -0700

    Removing irrelevant commented code

--- src/Simulator.cc
+++ src/Simulator.cc
@@ -6,9 +6,6 @@ namespace HeatFlow {
 	}
 	Simulator::Simulator(const HeatFlowProject& project_config) {
 		this->project_config_ = project_config;
-		//this->previous_temps_ = new MatrixFile<temperature_t>(this->project_config_.get_initial_temps());
-		//this->project_config_.copy_materials_matrix(this->materials_);
-		//this->materials_ = new MatrixFile<Material *>(this->project_config_.get_materials_matrix());
 		this->elapsed_time_ = 0.0;
 	}
 	
