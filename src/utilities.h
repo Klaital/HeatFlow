76ac665477e4cb9b9f391a3d8ddb8d4dde0899bf
Chris Cox <chriscox@abandonedfactory.net>
2019-01-28 11:16:44 -0800

    Removing C++ version

--- src/utilities.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEATFLOW_UTILITIES_H_
-#define HEATFLOW_UTILITIES_H_
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> string_split(const std::string& str, const std::string& delim);
-#endif
